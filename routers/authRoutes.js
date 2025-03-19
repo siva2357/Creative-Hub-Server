32efdb177457131188874c111935f2653bc79eed
Siva Prasad Kurra <sivakurra.ksp2357@gmail.com>
2025-03-19 19:06:34 +0530

    made chnsge sin route for uaht

--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -32,16 +32,16 @@ router.get('/auth/admin/:id', identifier, adminController.getAdminById);
 router.post('/auth/recruiter/signup', recruiterController.signup);
 router.patch('/auth/recruiter/:id/change-password', identifier, recruiterController.changePassword);
 router.post('/auth/recruiter/signout', identifier, recruiterController.signout);
-router.get('/auth/recruiters', recruiterController.getAllRecruiters);
-router.get('/auth/recruiter/:id', identifier, recruiterController.getRecruiterById);
+router.get('/recruiters', recruiterController.getAllRecruiters);
+router.get('/recruiter/:id', identifier, recruiterController.getRecruiterById);
 router.delete('/auth/recruiter/:id/delete', identifier, recruiterController.deleteRecruiterById);
 
 // ========================= Seeker Routes =========================
 router.post('/auth/seeker/signup', seekerController.signup);
 router.patch('/auth/seeker/:id/change-password', identifier, seekerController.changePassword);
 router.post('/auth/seeker/signout', identifier, seekerController.signout);
-router.get('/auth/seekers', seekerController.getAllSeekers);
-router.get('/auth/seeker/:id', identifier, seekerController.getSeekerById);
+router.get('/seekers', seekerController.getAllSeekers);
+router.get('/seeker/:id', identifier, seekerController.getSeekerById);
 router.delete('/auth/seeker/:id/delete', identifier, seekerController.deleteSeekerById);
 
 module.exports = router;
