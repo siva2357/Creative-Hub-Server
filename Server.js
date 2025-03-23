acd09ddbe300bfbab471ddd9447356b83b5cad75
Siva Prasad Kurra <sivakurra.ksp2357@gmail.com>
2025-03-23 22:59:57 +0530

    changes odneis erver

--- Server.js
+++ Server.js
@@ -17,16 +17,11 @@ const projectUploadRouter = require("./project-upload/projectUploadRoutes");
 const app = express();
 
 
-
-
 app.use(cors({
     origin: ['http://localhost:4200', 'https://creative-hub-8d4da.web.app'], // Allow both local and deployed frontend URLs
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true, // Allow credentials (cookies, headers)
 }));
-
-
-
 
 
 app.use(helmet({
